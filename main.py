07ca653eb9684217502fb22024e7c1dddde02d99
Peter Berna <peterbernaw@gmail.com>
2022-08-15 11:18:53 +0200

    Features:New GETs

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from tokenize import Name
 from flask import Flask, jsonify
 import random
 import tools.sqltools as sql 
@@ -10,17 +11,58 @@ app = Flask(__name__)
 def greeting ():
     return f"Hi! How are you doing?"
 
+#SUBSTITUTE GREETING BY 
+# GET: render markdown
+# @app.route("/")
+# def index():
+    # readme_file = open("README.md", "r")
+    # md_template = markdown.markdown(readme_file.read(), extensions = ["fenced_code"])
+    # return md_template
+
 # Get everything: SQL
 @app.route("/all")
 def all_reviews ():
+    print("These are all the reviews from the database:")
     return jsonify(sql.get_everything())
 
+# Get branches' names
+@app.route("/branch")
+def branches_names ():
+    print("These are the three branch names:")
+    return jsonify(sql.get_branch_names())
+
 # Get everything from 1 disney branch: SQL
-@app.route("/branch/<branch_name>")
-def all_from_branch ():
-    return jsonify(sql.get_everything_from_branch())
+@app.route("/branch/<name>")
+def all_from_branch (name):
+    print(f"These are all the reviews from {name}")
+    return jsonify(sql.get_everything_from_branch(name))
+
+# Get total nº of reviews
+@app.route("/total/reviews")
+def total_reviews ():
+    return jsonify(sql.get_total_reviews())
+
+#Get all reviewers' location
+@app.route("/all/locations")
+def all_locations ():
+    note = "These are all the reviewers' countries' of origin:"
+    result = sql.get_all_reviewer_location()
+    result["0"] = note
+    return jsonify(result)
+
+@app.route("/total/<country>")
+def total_one_country (country):
+    return jsonify(sql.get_total_one_country(country))
+
+@app.route("/all/reviews/<country>")
+def all_reviews_one_country(country):
+    note = f"These are all the reviews of visitors from {country}"
+    result = sql.get_reviews_one_country(country)
+    result["0"] = note
+    return jsonify(result)
+
 
 #this will check that the name is the meain
 if __name__ == '__main__': 
-    app.run(port=3306, debug=True)
+    app.run(port=8000, debug=True)
     # debug= True/False, when you change something it updatescle
--- tools/sqltools.py
+++ tools/sqltools.py
@@ -2,20 +2,44 @@ from config.sqlconfig import engine
 import pandas as pd
 
 ## GET
-def get_everything ():
-
-    query = (f"""SELECT *FROM users;""")
+def get_everything():
+    query = (f"""SELECT * FROM disneyland_reviews;""")
     df=pd.read_sql_query(query,con=engine)
     return df.to_dict(orient='records')
 
-def get_everything_from_someone(name):
-
-    query = (f"""SELECT * FROM users WHERE character_name = "{name}";""")
+def get_branch_names():
+    query = (f"""SELECT DISTINCT Branch FROM disneyland_reviews""")
     df=pd.read_sql_query(query,con=engine)
     return df.to_dict(orient='records')
 
+def get_everything_from_branch(name):
+    query = (f"""SELECT * FROM disneyland_reviews WHERE Branch = "{name}";""")
+    df=pd.read_sql_query(query,con=engine)
+    return df.to_dict(orient='records')
 
-## POST
+def get_total_reviews():
+    query = ("""SELECT COUNT(Review_text) as 'Total reviews' FROM disneyland_reviews;""")
+    df=pd.read_sql_query(query,con=engine)
+    return df.to_dict(orient='records')
+
+def get_all_reviewer_location():
+    query = (f"""SELECT DISTINCT Reviewer_Location FROM disneyland_reviews""")
+    df=pd.read_sql_query(query,con=engine)
+    return df.to_dict()
+
+def get_total_one_country(country):
+    country = f'"{country}"'
+    query = (f"""SELECT COUNT(Review_text) as 'Total reviews from {country}' FROM disneyland_reviews WHERE Reviewer_Location = {country}""")
+    df=pd.read_sql_query(query,con=engine)
+    return df.to_dict(orient='records')
+
+def get_reviews_one_country(country):
+    country = f'"{country}"'
+    query = (f"""SELECT Review_Text FROM disneyland_reviews WHERE Reviewer_Location = {country}""")
+    df=pd.read_sql_query(query,con=engine)
+    return df.to_dict()
+
+    ## POST
 def new_message (scene, character_name, dialogue):
 
     engine.execute(f"""
